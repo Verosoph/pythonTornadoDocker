3f2f9259b2cffd47dabd06d412e4c29baa58b041
Marco Mettke <mrm@binyamen.ubuntu1604.wk.tb>
2018-04-26 12:49:09 +0200

    without autoload

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
     application.listen(port)
 
     #TODO remove in prod
-    tornado.autoreload.start()
-    for dir, _, files in os.walk('static'):
-        [tornado.autoreload.watch(dir + '/' + f) for f in files if not f.startswith('.')]
+    #tornado.autoreload.start()
+    #for dir, _, files in os.walk('static'):
+     #   [tornado.autoreload.watch(dir + '/' + f) for f in files if not f.startswith('.')]
 
 
     tornado.ioloop.IOLoop.instance().start()
