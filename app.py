189636b3437f58feb67895accd84f2011c33356c
Marco Mettke <mrm@binyamen.ubuntu1604.wk.tb>
2018-04-26 12:46:15 +0200

    autoreload

--- app.py
+++ app.py
@@ -21,9 +21,20 @@ application = tornado.web.Application([
 
 #Start the server at a port
 port = 8889
+
+
+
 if __name__ == "__main__":
+    
     print ("Server is running on 8889")
     print ("Press ctrl+c to close")
     application.listen(port)
+
+    #TODO remove in prod
+    tornado.autoreload.start()
+    for dir, _, files in os.walk('static'):
+        [tornado.autoreload.watch(dir + '/' + f) for f in files if not f.startswith('.')]
+
+
     tornado.ioloop.IOLoop.instance().start()
 
